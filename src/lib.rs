8d1c9d19a059e117f5dc61714d36f93bf00bbecb
enomoto11 <enomoto_togo@cyberagent.co.jp>
2025-05-16 14:41:38 +0900

    RustとWebAssemblyプロジェクトのためのテンプレートを追加。AppVeyorとTravis CIの設定ファイルを作成し、必要な依存関係を追加。基本的なREADMEとライセンスファイルも含む。

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// Rustのexternは「どの言語の呼び出し規約（ABI）を使うか」を指定します。
+// ここではC言語のABI（Application Binary Interface）を指定しています。
+// Rustにはextern "JS"は存在せず、wasm-bindgenがこのextern "C"の関数をJavaScriptに橋渡ししてくれます。
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm-game-of-life!");
+}
