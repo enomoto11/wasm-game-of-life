a4068913b2ff26ee715bee18e637aac870e1e0e6
enomoto11 <enomoto_togo@cyberagent.co.jp>
2025-05-18 01:06:03 +0900

    index.htmlにスタイルを追加し、ゲームのキャンバスを表示するための要素を追加。index.jsでゲームのループを実装し、描画と進化を行うように変更。

--- www/index.js
+++ www/index.js
@@ -1,3 +1,14 @@
 import * as wasm from "wasm-game-of-life";
 
-wasm.greet("togo");
+// wasm.greet("togo");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = wasm.Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    requestAnimationFrame(renderLoop);
+};
+
+renderLoop();
