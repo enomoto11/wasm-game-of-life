ef66d35952605a4ef2a96c2c82c0b678e689448f
enomoto11 <enomoto_togo@cyberagent.co.jp>
2025-05-18 01:28:35 +0900

    依存関係にjs-sysを追加し、ランダムなセルの生成にMath::random()を使用するように変更。

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod utils;
-
+use js_sys::Math;
 use std::fmt;
 use wasm_bindgen::prelude::*;
 
@@ -109,8 +109,8 @@ impl Universe {
         let height = 64;
 
         let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
+            .map(|_| {
+                if Math::random() < 0.5 {
                     Cell::Alive
                 } else {
                     Cell::Dead
